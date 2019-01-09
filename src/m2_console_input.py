4d674f37b247321956128eeca53e62700ffc77ab
swartdc <swartdc@rose-hulman.edu>
2019-01-09 10:36:54 -0500

    DNOE

--- src/m2_console_input.py
+++ src/m2_console_input.py
@@ -5,7 +5,7 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
          and Derrick Swart.
 """  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
-
+import math
 
 def main():
     """ TESTs the functions in this module (by calling them). """
@@ -31,10 +31,11 @@ def double_a_float():
          -6.28
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    x = float(input('enter an integer to be doubled   '))
+    print (x * 2)
 
 def print_an_integer_many_times():
     """
@@ -62,10 +63,12 @@ def print_an_integer_many_times():
          10
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
-
+    x = int(input('enter integer   '))
+    for _ in range(x):
+        print (x * 2)
 
 def print_an_integer_many_times_on_one_row():
     """
@@ -81,13 +84,14 @@ def print_an_integer_many_times_on_one_row():
          1010101010
     """
     # -------------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #   The testing code is already written for you (above).
     #
     # HINT: One way to print on a SINGLE line is to build up a string
     #       and then print that (single) string.
     # -------------------------------------------------------------------------
-
+    x = int(input('enter integer  '))
+    print(str((x*2))*x)
 
 def input_it_all():
     """
@@ -121,9 +125,16 @@ def input_it_all():
          Peace & Love.
     """
     # -------------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #   The testing code is already written for you (above).
     # -------------------------------------------------------------------------
+    x = float(input(' enter float for sqrt         '))
+    y = int(input('enter interger for iterations   '))
+    z = input('enter string to be repeated         ')
+    for _ in range (y):
+        print((math.sqrt(x)))
+    for i in range (y):
+        print(z)
 
 
 # -----------------------------------------------------------------------------
